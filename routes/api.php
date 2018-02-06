37c6ef4b5e90f9ed75d5303f2159dc3dc91dec57
[Ahmed Mahmoud] <amahmoud033@gmail.com>
2018-02-06 15:31:25 +0300

    create model , controller , migrations and factories

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/programs', 'ProgramController');
+
+Route::group(['prefix'=>'programs'], function (){
+   Route::apiResource('/{program}/exercises', 'ExerciseController');
+});
